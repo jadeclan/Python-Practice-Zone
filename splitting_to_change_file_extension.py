a749022bdfc3ad67d9f7f5a183d23e17762abc0a
James <jberg475@mtroyal.ca>
2025-09-18 11:18:30 -0600

    adding json extension to a filename

--- splitting_to_change_file_extension.py
+++ splitting_to_change_file_extension.py
@@ -1,10 +1,13 @@
 def main():
     # This algorithm was tested with no extension and just a period -> Both worked.
     filename = 'this_file.txt'
+
     removed_extension = filename.rsplit('.',1)
     extensionless_filename = removed_extension[0]
     new_name = extensionless_filename + '.json'
 
+    test = filename.rsplit('.',1)[0] + '.json' # <- This line does the same as the previous 3 lines
     print(f"Old file name was: {filename}. The new file is now {new_name}.")
+    print(f"Old file name was: {filename}. The new file is now {test}.")
 
 main()
