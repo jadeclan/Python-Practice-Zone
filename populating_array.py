afaf6a03ea928e218d3a08d4d530d3d016bcf29e
James <jberg475@mtroyal.ca>
2025-10-30 19:01:53 -0600

    Numpy practice

---
+++ populating_array.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+maxi = 0
+mini = 0
+for i in range(1000000):
+    data = np.random.rand(2,3)*2 - 1  # Random values between -1 and 1
+    if np.max(data) > maxi:
+        maxi = np.max(data)
+    if np.min(data) < mini:
+        mini = np.min(data)
+
+print(mini, maxi)
+
+arr = np.arange(5)
+print(np.exp(arr))  # Exponential of each element in the array
